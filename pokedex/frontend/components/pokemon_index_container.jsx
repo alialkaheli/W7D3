940d493d376ed189165f36e3713e272daa134077
alialkaheli <ali.alkaheli96@gmail.com>
2018-12-06 15:17:30 -0800

    Rename mapStateToProps and correct file path

--- pokedex/frontend/components/pokemon_index_container.jsx
+++ pokedex/frontend/components/pokemon_index_container.jsx
@@ -1,14 +1,17 @@
 import {connect} from 'react-redux';
-import selectAllPokemon from './reducers/selectors';
+import selectAllPokemon from '../reducers/selectors';
 import PokemonIndex from './pokemon_index';
 import {requestAllPokemon} from '../actions/pokemon_actions';
 
 const mapStateToProps = (state) => ({
-  pokemon: selectAllPokemon(state)
+  arrayofpoke: selectAllPokemon(state)
 });
 
 const mapDispatchToProps = dispatch => ({
-  requestAllPokemon:  dispatch(requestAllPokemon)
+  requestAllPokemon: () => dispatch(requestAllPokemon())
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(PokemonIndex);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PokemonIndex);

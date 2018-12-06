e3ae41ccb558576132a651c3726e43e18af9a33f
alialkaheli <ali.alkaheli96@gmail.com>
2018-12-06 11:35:31 -0800

    Import APIUtil AJAX function and create thunk action creator

--- pokedex/frontend/actions/pokemon_actions.js
+++ pokedex/frontend/actions/pokemon_actions.js
@@ -1,3 +1,4 @@
+import * as APIUtil from '../util/api_util';
 export const RECEIVE_ALL_POKEMON = "RECEIVE_ALL_POKEMON";
 
 export const receiveAllPokemon = pokemon => {
@@ -6,3 +7,8 @@ export const receiveAllPokemon = pokemon => {
     pokemon: pokemon,
   });
 };
+
+export const requestAllPokemon = () => (dispatch) => (
+  APIUtil.fetchAllPokemon()
+    .then(pokemon => dispatch(receiveAllPokemon(pokemon)))
+);

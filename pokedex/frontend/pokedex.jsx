17ad3f0a543de06af0c3112cae6a81326c9a7b08
alialkaheli <ali.alkaheli96@gmail.com>
2018-12-05 17:05:12 -0800

    Import apiUtil function and receiveAllPokemon action, set window context

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {fetchAllPokemon}from './util/api_util';
+import {receiveAllPokemon} from './actions/pokemon_actions';
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
   ReactDOM.render(<h1>You done goofed Daddy</h1>, rootEl);
 });
+window.fetchAllPokemon = fetchAllPokemon;
+window.receiveAllPokemon = receiveAllPokemon;

edcd665c7df1c81b9c625fd3bc32eaee7f3fdf3b
alialkaheli <ali.alkaheli96@gmail.com>
2018-12-06 11:36:15 -0800

    Import all pokemon actions, make dispatch, getState, store and requestAllPokemon available on window

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {fetchAllPokemon}from './util/api_util';
-import {receiveAllPokemon} from './actions/pokemon_actions';
+import {receiveAllPokemon, requestAllPokemon} from './actions/pokemon_actions';
+import configureStore from './store/store';
+
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
+  const store = configureStore();
   ReactDOM.render(<h1>You done goofed Daddy</h1>, rootEl);
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 });
+
 window.fetchAllPokemon = fetchAllPokemon;
 window.receiveAllPokemon = receiveAllPokemon;
+window.requestAllPokemon = requestAllPokemon;

4769cfe9143487c97f9e0ab9d155baa4778cea9f
alialkaheli <ali.alkaheli96@gmail.com>
2018-12-06 11:38:31 -0800

    Comment out itemsReducer import and entityReducer key

--- pokedex/frontend/reducers/entities_reducer.js
+++ pokedex/frontend/reducers/entities_reducer.js
@@ -1,10 +1,10 @@
 import pokemonReducer from './pokemon_reducer';
 import {combineReducers} from 'redux';
-import itemsReducer from './items_reducer';
+// import itemsReducer from './items_reducers';
 
 const entitiesReducer = combineReducers({
   pokemon: pokemonReducer,
-  items: itemsReducer,
+  // items: itemsReducer,
 });
 
 export default entitiesReducer;

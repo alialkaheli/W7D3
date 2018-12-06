b11ba46c33044b3652b66cfe2686274ebd074d2a
alialkaheli <ali.alkaheli96@gmail.com>
2018-12-06 14:12:54 -0800

    Add Root container

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -3,15 +3,23 @@ import ReactDOM from 'react-dom';
 import {fetchAllPokemon}from './util/api_util';
 import {receiveAllPokemon, requestAllPokemon} from './actions/pokemon_actions';
 import configureStore from './store/store';
+import selectAllPokemon from './reducers/selectors';
+import {Provider} from 'react-redux';
+
+const Root = ({store}) => (
+  <Provider store={store}>
+    <div>Hello world!</div>
+  </Provider>
+);
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
   const store = configureStore();
-  ReactDOM.render(<h1>You done goofed Daddy</h1>, rootEl);
+  ReactDOM.render(<Root store={store}/>, rootEl);
   window.getState = store.getState;
   window.dispatch = store.dispatch;
 });
-
+window.selectAllPokemon = selectAllPokemon;
 window.fetchAllPokemon = fetchAllPokemon;
 window.receiveAllPokemon = receiveAllPokemon;
 window.requestAllPokemon = requestAllPokemon;

9df294cbd43ef2b0eaa1472629feaedf63a243d4
alialkaheli <ali.alkaheli96@gmail.com>
2018-12-06 16:59:21 -0800

    Add Link tag for each Pokemon

--- pokedex/frontend/components/pokemon/pokemon_index_item.jsx
+++ pokedex/frontend/components/pokemon/pokemon_index_item.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const PokemonIndexItem = ({poke}) => (
-  <li>{poke.name}
-    <img src={poke.image_url}/></li>
+  <li>
+    <Link to={`/pokemon/${poke.id}`}>{poke.name} <img src={poke.image_url}/></Link>
+  </li>
 );
 
 export default PokemonIndexItem;

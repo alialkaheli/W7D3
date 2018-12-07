c6167c26d2c98ea5f22e3433556629b4f9717c01
alialkaheli <ali.alkaheli96@gmail.com>
2018-12-06 16:28:29 -0800

    Modify file paths for selectAllPokemon selector and Pokemon actions

--- pokedex/frontend/components/pokemon/pokemon_index_container.jsx
+++ pokedex/frontend/components/pokemon/pokemon_index_container.jsx
@@ -1,7 +1,7 @@
 import {connect} from 'react-redux';
-import selectAllPokemon from '../reducers/selectors';
+import selectAllPokemon from '../../reducers/selectors';
 import PokemonIndex from './pokemon_index';
-import {requestAllPokemon} from '../actions/pokemon_actions';
+import {requestAllPokemon} from '../../actions/pokemon_actions';
 
 const mapStateToProps = (state) => ({
   arrayofpoke: selectAllPokemon(state)
